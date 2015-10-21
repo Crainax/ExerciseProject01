4665fcfbb01d120b750a45801eb26d607bc069c1
Ruffneck <413997411@qq.com>
2015-10-21 22:49:03 +0800

    interpolator shake animation!!!!!

--- app/src/main/java/com/ruffneck/mobilesafer/activity/AddressActivity.java
+++ app/src/main/java/com/ruffneck/mobilesafer/activity/AddressActivity.java
@@ -2,10 +2,17 @@ package com.ruffneck.mobilesafer.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.Interpolator;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ruffneck.mobilesafer.R;
 import com.ruffneck.mobilesafer.db.dao.AddressDAO;
@@ -25,6 +32,25 @@ public class AddressActivity extends Activity {
 
         et_number = (EditText) findViewById(R.id.et_number);
         tv_result = (TextView) findViewById(R.id.tv_result);
+
+        et_number.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                System.out.println(s);
+                String address = AddressDAO.getAddress(s.toString());
+                tv_result.setText(address);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
     }
 
 
@@ -35,6 +61,12 @@ public class AddressActivity extends Activity {
         if (!TextUtils.isEmpty(number)) {
             String address = AddressDAO.getAddress(number);
             tv_result.setText(address);
+        }else{
+            Animation animation = AnimationUtils.loadAnimation(this, R.anim.shake);
+
+            BounceInterpolator al;
+            et_number.startAnimation(animation);
+            Toast.makeText(AddressActivity.this, "输入框不能为空", Toast.LENGTH_SHORT).show();
         }
     }
 }
